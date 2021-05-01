3d680ee19851e6f2a4caccef03feac2d1a68a90a
Hector Martin <marcan@marcan.st>
2021-05-02 03:47:15 +0900

    export_joysound_prj.py: Many improvements
    Now produces no warnings in the Joysound app log, and the project file
    is almost identical after re-saving.

--- export_joysound_prj.py
+++ export_joysound_prj.py
@@ -21,8 +21,11 @@ import time
 import subprocess
 import argparse
 import hashlib
+import unicodedata
 
+from io import BytesIO
 import xml.etree.ElementTree as ET
+from xml.dom import minidom
 
 from blitzloop import graphics, layout, mpvplayer, song, util
 
@@ -44,8 +47,8 @@ opts.mpv_vo = "null"
 s = song.Song(opts.songpath)
 
 # We need a virtual screen for line layout, just use something
-WIDTH = 1280
-HEIGHT = 720
+WIDTH = 720
+HEIGHT = 480
 
 class JoysoundProject(object):
     def __init__(self, song):
@@ -58,21 +61,21 @@ class JoysoundProject(object):
                 </music_info>
                 <telop/>
             </kyokupro>
-        """)
+        """.replace("\n", "").replace(" ", ""))
         self.et = ET.ElementTree(self.root)
-        
+
         if song:
             self.load(song)
-    
+
     def _tag(self, name, val):
         e = ET.Element(name)
         e.text = val
         return e
-    
+
     def _load_music_info(self, song):
         info = self.root.find("music_info")
         telop = self.root.find("telop")
-        
+
         if "title" in song.meta:
             info.append(self._tag("song_name", song.meta["title"][None]))
             telop.append(self._tag("song_name", song.meta["title"][None]))
@@ -80,20 +83,21 @@ class JoysoundProject(object):
                 info.append(self._tag("song_name_yomi", song.meta["title"]["k"]))
         if "artist" in song.meta:
             info.append(self._tag("artist_name", song.meta["artist"][None]))
-            telop.append(self._tag("artist_name", song.meta["artist"][None]))
+            telop.append(self._tag("artist_name", "♪ " + song.meta["artist"][None]))
             if "k" in song.meta["artist"]:
                 info.append(self._tag("artist_name_yomi", song.meta["artist"]["k"]))
-        if "composer" in song.meta:
-            info.append(self._tag("composer_name", song.meta["composer"][None]))
-            telop.append(self._tag("composer_name", song.meta["composer"][None]))
+        info.append(self._tag("original_artist_name", " "))
         if "writer" in song.meta:
             info.append(self._tag("lyricist_name", song.meta["writer"][None]))
-            telop.append(self._tag("lyricist_name", song.meta["writer"][None]))
+            telop.append(self._tag("lyricist_name", "作詞 " + song.meta["writer"][None]))
+        if "composer" in song.meta:
+            info.append(self._tag("composer_name", song.meta["composer"][None]))
+            telop.append(self._tag("composer_name", "作曲 " + song.meta["composer"][None]))
+        info.append(self._tag("cover_code", " "))
 
     def _load_audio(self, song, tag, path, **extra):
         el = ET.XML("""
             <music>
-                <set>1</set>
                 <file_path/>
                 <md5_sum/>
                 <type>1</type>
@@ -103,18 +107,38 @@ class JoysoundProject(object):
                 <vol>50</vol>
                 <balance>0</balance>
                 <reverb>0</reverb>
-                <enable>1</enable>
             </music>
-        """)
+        """.replace("\n", "").replace(" ", ""))
         el.tag = tag
         el.find("file_path").text = opts.pathbase + path
         h = hashlib.md5(open(os.path.join(song.pathbase, path), "rb").read()).hexdigest()
         el.find("md5_sum").text = h
-        el.find("play_time").text = "%d" % (int(self.duration * 1000))
+        el.find("play_time").text = "%d" % (int(self.duration))
         for k,v in extra.items():
             el.append(self._tag(k, v))
         return el
 
+    def _load_video(self, song, path, **extra):
+        el = ET.XML("""
+            <movie>
+                <file_path/>
+                <md5_sum/>
+                <type>1</type>
+                <format>4</format>
+                <play_time/>
+                <begin_time>0</begin_time>
+                <end_time/>
+                <enable>1</enable>
+            </movie>
+        """.replace("\n", "").replace(" ", ""))
+        el.find("file_path").text = opts.pathbase + path
+        h = hashlib.md5(open(os.path.join(song.pathbase, path), "rb").read()).hexdigest()
+        el.find("md5_sum").text = h
+        el.find("play_time").text = "%d" % (int(self.duration))
+        el.find("end_time").text = "%d" % (int(self.duration))
+        for k,v in extra.items():
+            el.append(self._tag(k, v))
+        return el
     def _load_resources(self, song):
         if "cover" in song.song:
             picture = ET.Element("picture")
@@ -123,14 +147,24 @@ class JoysoundProject(object):
             picture.append(self._tag("artist_md5_sum", h))
             self.root.append(picture)
         if "audio_instrumental" in song.song:
-            self.root.append(self._load_audio(song, "music", song.song["audio_instrumental"], set="1"))
+            music = self._load_audio(song, "music", song.song["audio_instrumental"], enable="1")
+            music.insert(0, self._tag("set", "1"))
+            self.root.append(music)
         if "audio_vocal" in song.song:
             vocal = ET.Element("vocal")
             vocal.append(self._tag("set", "1"))
-            vocal.append(self._load_audio(song, "song", song.song["audio_vocal"], song_type="0", song_number="0", marker_count="0"))
+            vocal.append(self._load_audio(song, "song", song.song["audio_vocal"],
+                                          song_type="0", song_number="0", enable="1",  marker_count="0"))
             self.root.append(vocal)
         if "audio" in song.song:
-            self.root.append(self._load_audio(song, "mix", song.song["audio"], set="1"))
+            mix = self._load_audio(song, "mix", song.song["audio"], enable="1")
+            mix.insert(0, self._tag("set", "1"))
+            self.root.append(mix)
+        if "video" in song.song:
+            back = ET.Element("back")
+            back.append(self._tag("set", "1"))
+            back.append(self._load_video(song, song.song["video"]))
+            self.root.append(back)
 
     def _load_lyrics(self, s):
         telop = self.root.find("telop")
@@ -146,6 +180,10 @@ class JoysoundProject(object):
         def ms(i):
             return str(int(round(i * 1000)))
 
+        def color(rgb):
+            r, g, b = rgb
+            return "%d" % (r | (g << 8) | (b << 16))
+
         # Group lines into pages
         pages = []
         page = Page()
@@ -159,7 +197,7 @@ class JoysoundProject(object):
             page.lines[l.row] = l
             last = l
         pages.append(page)
-        
+
         # Compute page show/hide times
         prev = None
         pages2 = []
@@ -179,7 +217,7 @@ class JoysoundProject(object):
                         raise Exception("overlapping lines! %r" % (li))
             prev = page
             pages2.append(page)
-        
+
         # Generate XML
         for page in pages:
             pe = ET.Element("page")
@@ -189,53 +227,99 @@ class JoysoundProject(object):
             pe.append(self._tag("layout", "xing_0"))
 
             t = page.min_start
+            styles = None
             for i in range(max(page.lines.keys()), -1, -1):
-                
                 le = ET.Element("line")
                 text = ""
                 if i not in page.lines:
+                    if styles:
+                        styles = [[styles[-1][0], 0, 0]]
+                    else:
+                        styles = [[page.lines.values()[0].molecules[0].style, 0, 0]]
                     # dummy line
                     w = ET.Element("word")
-                    w.append(self._tag("text", ""))
+                    w.append(self._tag("text", " "))
                     w.append(self._tag("start_time", ms(t)))
                     w.append(self._tag("end_time", ms(t)))
                     le.append(w)
+                    text = " "
+                    width = cwidth = 0
+                    align = 0
                 else:
                     line = page.lines[i]
-                    assert len(line.molecules) == 1
-                    mol, get_atom_time = line.molecules[0]
+                    align = line.align
                     ruby = []
-                    step = 0
-                    for atom in mol.atoms:
-                        start, end = get_atom_time(step, atom.steps)
-                        step += atom.steps
+                    styles = []
 
-                        w = ET.Element("word")
-                        w.append(self._tag("text", atom.text))
-                        w.append(self._tag("start_time", ms(start)))
-                        w.append(self._tag("end_time", ms(end)))
-                        le.append(w)
-                        
-                        if atom.particles is not None:
-                            edge = len(atom.text)
-                            if atom.particle_edge:
-                                edge = atom.particle_edge 
-                            rt = ""
-                            for i in atom.particles:
-                                rt += i.text
-                            r = ET.Element("ruby")
-                            r.append(self._tag("text", rt))
-                            r.append(self._tag("start_pos", "%d" % len(text)))
-                            r.append(self._tag("end_pos", "%d" % (len(text) + edge - 1)))
-                            ruby.append(r)
-                        
-                        text += atom.text
+                    for idx, instance in enumerate(line.molecules):
+                        mol = instance.molecule
+                        step = 0
+                        start_pos = len(text)
+                        for atom in mol.atoms:
+                            start, end = instance.get_atom_time(step, atom.steps)
+                            t = max(t, end)
+                            step += atom.steps
+
+                            w = ET.Element("word")
+                            w.append(self._tag("text", atom.text))
+                            w.append(self._tag("start_time", ms(start)))
+                            w.append(self._tag("end_time", ms(end)))
+                            le.append(w)
+
+                            if atom.particles is not None:
+                                edge = len(atom.text)
+                                edge_l = 0
+                                if atom.particle_edge:
+                                    edge = atom.particle_edge
+                                if atom.particle_edge_l:
+                                    edge_l = atom.particle_edge_l
+                                rt = ""
+                                for i in atom.particles:
+                                    rt += i.text
+                                r = ET.Element("ruby")
+                                r.append(self._tag("text", rt))
+                                r.append(self._tag("start_pos", "%d" % (len(text) + edge_l)))
+                                r.append(self._tag("end_pos", "%d" % (len(text) + edge - 1)))
+                                ruby.append(r)
+
+                            text += atom.text
+                        if idx != (len(line.molecules) - 1):
+                            text += "　"
+                            w[0].text += "　"
+                        if styles and instance.style == styles[-1]:
+                            styles[-1][2] = len(text) - 1
+                        else:
+                            styles.append([instance.style, start_pos, len(text) - 1])
 
                     for r in ruby:
                         le.append(r)
 
                     print(text)
-                
+                    width = line.max_px - line.min_px
+                    cwidth = len(text)
+                    for i in text:
+                        ew = unicodedata.east_asian_width(i)
+                        if ew in ("F", "W", "A"):
+                            cwidth += 1
+
+                    if cwidth > 26:
+                        print("  ^-- WARNING: Line likely too long")
+
+                for style, start, end in styles:
+                    e = ET.Element("color")
+                    e.append(self._tag("before_text_color", color(style.colors[0])))
+                    e.append(self._tag("after_text_color", color(style.colors_on[0])))
+                    e.append(self._tag("before_shadow_color", color(style.colors[1])))
+                    e.append(self._tag("after_shadow_color", color(style.colors_on[1])))
+                    e.append(self._tag("before_text_no_fill_color", color(style.colors[0])))
+                    e.append(self._tag("after_text_no_fill_color", color(style.colors_on[0])))
+                    e.append(self._tag("before_shadow_no_fill_color", color(style.colors[1])))
+                    e.append(self._tag("after_shadow_no_fill_color", color(style.colors_on[1])))
+                    e.append(self._tag("start_pos", "%d" % start))
+                    e.append(self._tag("end_pos", "%d" % end))
+                    e.append(self._tag("no_fill", "1" if style.colors == style.colors_on else "0"))
+                    le.append(e)
+
                 e = ET.Element("duet")
                 e.append(self._tag("mark", "0"))
                 e.append(self._tag("start_pos", "0"))
@@ -243,14 +327,33 @@ class JoysoundProject(object):
                 le.append(e)
 
                 e = ET.Element("offset")
-                e.append(self._tag("type", "0"))
-                e.append(self._tag("offset", "0"))
+                w = 720
+                margin = 54
+                offset = margin + (w * (1 - width) - 2 * margin) * align
+                if offset < 0 or offset + w * width > w - margin:
+                    print("  ^-- WARNING: Line too wide")
+                # 0 = default
+                # 1 = offset from default
+                # 2 = abs left side
+                e.append(self._tag("type", "2"))
+                e.append(self._tag("auto_pos", "0"))
+                e.append(self._tag("offset", "%d" % round(offset)))
                 le.append(e)
 
                 pe.append(le)
-            
+
             telop.append(pe)
-            
+
+        self.root.remove(telop)
+        self.root.append(telop)
+
+        telop_edit_setting = ET.XML("""
+            <telop_edit_setting>
+                <music_volume>50</music_volume>
+                <vocal_volume>50</vocal_volume>
+            </telop_edit_setting>
+        """.replace("\n", "").replace(" ", ""))
+        self.root.append(telop_edit_setting)
 
     def load(self, song):
         self.display = graphics.Display(WIDTH, HEIGHT)
@@ -267,6 +370,11 @@ class JoysoundProject(object):
         self._load_lyrics(song)
 
 pro = JoysoundProject(s)
-pro.et.write(opts.output, encoding="UTF-8", xml_declaration=True)
+bio = BytesIO()
+pro.et.write(bio, encoding="UTF-8", xml_declaration=True)
+xmlstr = bio.getvalue().replace(b"\n", b" ").replace(b"\r", b"")
+xmlstr = minidom.parseString(xmlstr).toprettyxml(indent="  ", encoding="UTF-8").replace(b"\n", b"\r\n")
+with open(opts.output, "wb") as fd:
+    fd.write(xmlstr)
 
 os._exit(0)
